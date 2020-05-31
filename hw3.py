9d6df0da4570919448cda34e52160a4da265fb66
naumdaum <naumdaum@naver.com>
2020-05-31 22:22:48 +0900

    Create hw3.py
    3주차 숙제

---
+++ hw3.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta   
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+soup = BeautifulSoup(data.text, 'html.parser')
+songs = soup.select('#body-content > div.newest-list > div.music-list-wrap > table > tbody > tr')
+
+
+for song in songs:
+        a_tag = song.select_one('td.info > a.artist')
+        rank = song.select_one('td.number').text[:2].strip()                      # a 태그 사이의 텍스트를 가져오기
+        song_title = song.select_one('td.check> input.select-check')['title']
+        singer = a_tag.text
+        print(rank, song_title, singer)
+        
